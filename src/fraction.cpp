f97eb6eed9213ec9bcd311ca2fc92c65108d9d77
Andrey <suspendwinlogon@gmail.com>
2018-12-16 15:26:11 +0700

    Added Fraction constructors + fixed Fraction::reduce()

--- src/fraction.cpp
+++ src/fraction.cpp
@@ -3,8 +3,24 @@ using namespace MathAn;
 
 void Fraction::reduce() const {
   Num gcd = Operations::gcd(numerator, denominator);
-  if (gcd == 1)
+  if (gcd <= 2)
     return;
   numerator /= gcd;
   denominator /= gcd;
+}
+
+Fraction::Fraction() {
+  numerator = 1;
+  denominator = 1;
+}
+
+Fraction::Fraction(const Num num, const Num den) {
+  numerator = num;
+  denominator = den;
+  reduce();
+}
+
+Fraction(const Fraction other) {
+  numerator = other.numerator;
+  denominator = other.denominator;
 }
