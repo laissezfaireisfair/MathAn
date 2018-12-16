5e2f8f36fb43a7b0913fb6abc008f3d2ff719dfd
Andrey <suspendwinlogon+proj@gmail.com>
2018-12-16 14:46:35 +0700

    Added fraction header.

--- include/fraction.hpp
+++ include/fraction.hpp
@@ -1,5 +1,33 @@
 #include <cmath>
+#include <string>
+using namespace std;
 
 namespace MathAn {
-  class Fraction {};
+  class Fraction {
+  private:
+    typedef int Num;
+    Num numerator, denominator;
+    void reduce();
+  public:
+    Fraction();
+    Fraction(const Num num, const Num den);
+    Fraction(const Fraction other);
+    Fraction operator=(const Fraction other);
+    Fraction operator+(const Fraction other) const;
+    Fraction operator-(const Fraction other) const;
+    Fraction operator*(const Fraction other) const;
+    Fraction operator/(const Fraction other) const;
+    Fraction operator+=(const Fraction other);
+    Fraction operator-=(const Fraction other);
+    Fraction operator*=(const Fraction other);
+    Fraction operator/=(const Fraction other);
+    bool operator==(const Fraction other);
+    bool operator!=(const Fraction other);
+    bool operator<(const Fraction other);
+    bool operator>(const Fraction other);
+    bool operator<=(const Fraction other);
+    bool operator>=(const Fraction other);
+    double count() const;
+    string get_str() const;
+  };
 }
