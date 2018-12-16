fe029ad10bfd4d4594891f0ed1402e2194a859c9
Andrey <suspendwinlogon@gmail.com>
2018-12-16 15:20:52 +0700

    Added reduce function realisation to fraction.cpp

--- src/fraction.cpp
+++ src/fraction.cpp
@@ -1,2 +1,10 @@
-#include "fraction.hpp"
+#include "./../include/fraction.hpp"
 using namespace MathAn;
+
+void Fraction::reduce() const {
+  Num gcd = Operations::gcd(numerator, denominator);
+  if (gcd == 1)
+    return;
+  numerator /= gcd;
+  denominator /= gcd;
+}
