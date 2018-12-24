093ccbe49eaf6f443c1a0f16be50b18614974c47
Andrey <suspendwinlogon@gmail.com>
2018-12-24 22:09:05 +0700

    Fixed namespace in operations.cpp

--- src/operations.cpp
+++ src/operations.cpp
@@ -4,5 +4,5 @@ using namespace MathAn;
 static template<class Num> Num Operations::gcd(const Num a, const Num b) {
   if (b == 0)
 		return a;
-	return gcd(b, a % b);
+	return Operations::gcd(b, a % b);
 }
