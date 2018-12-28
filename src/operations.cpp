737e8f4bd4a08296932062863a9047a4f09c2f41
Andrey <suspendwinlogon@gmail.com>
2018-12-28 18:07:14 +0700

    Fixed namespaces in operations.cpp

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,7 +1,7 @@
 #include "./../include/operations.hpp"
 using namespace MathAn;
 
-static template<class Num> Num Operations::gcd(const Num a, const Num b) {
+static template<class Num> Num MathAn::Operations::gcd(const Num a, const Num b) {
   if (b == 0)
 		return a;
 	return Operations::gcd(b, a % b);
