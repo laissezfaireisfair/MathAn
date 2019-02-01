a8f862d23ea627e8a6da581d48d979a89aa23afa
Andrey <suspendwinlogon@gmail.com>
2019-02-01 12:50:55 +0700

    Added first tests

---
+++ test/testFractions.cpp
@@ -0,0 +1,20 @@
+#include "./../include/fraction.hpp"
+#include "./../src/fraction.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+  MathAn::Fraction a(1, 2), b(2, 3);
+  cout << "a = " << a.get_str() << "; b = " << b.get_str() << ";" << endl;
+  cout << "a + b = " << MathAn::Fraction(a + b).get_str() << ";" << endl;
+  cout << "a - b = " << MathAn::Fraction(a - b).get_str() << ";" << endl;
+  cout << "a * b = " << MathAn::Fraction(a * b).get_str() << ";" << endl;
+  cout << "a / b = " << MathAn::Fraction(a / b).get_str() << ";" << endl;
+  cout << "a > b = " << (a > b ? "true" : "false") << ";" << endl;
+  cout << "a < b = " << (a < b ? "true" : "false") << ";" << endl;
+  cout << "a >= b = " << (a >= b ? "true" : "false") << ";" << endl;
+  cout << "a <= b = " << (a <= b ? "true" : "false") << ";" << endl;
+  cout << "a == b = " << (a == b ? "true" : "false") << ";" << endl;
+  cout << "a != b = " << (a != b ? "true" : "false") << ";" << endl;
+  return 0;
+}
