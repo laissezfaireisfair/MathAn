b77646f1ef0bf35705eab1ba51a606723ae53952
Andrey <suspendwinlogon@gmail.com>
2019-02-02 16:36:40 +0700

    Added first Point2 test

---
+++ test/testGeometryPoint2.cpp
@@ -0,0 +1,11 @@
+#include "./../include/geometry.hpp"
+#include "./../src/geometry.cpp"
+#include <iostream>
+using namespace std;
+using namespace MathAn;
+
+int main() {
+  Point2 a;
+  cout << "a.x = " << a.get_x() << "; a.y = " << a.get_y() << ";" << endl;
+  return 0;
+}
