beff8c6fbc90dc0ef6503309c9eae1052a4a986a
Andrey <suspendwinlogon@gmail.com>
2019-02-02 14:19:03 +0700

    Added <cmath> include in geometry.hpp and get_normalised() and get_length() declarations switched places.

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cmath>
 
 namespace MathAn {
   class Vector2 {
@@ -7,8 +8,8 @@ namespace MathAn {
     Vector2();
     Vector2(const double xi, const double yi);
     Vector2(const Vector2 i);
-    Vector2 get_normalised() const;
     double get_length() const;
+    Vector2 get_normalised() const;
     Vector2 operator+(const Vector2 other) const;
     Vector2 operator-(const Vector2 other) const;
     Vector2 operator*(const Vector2 other) const;
