3ed9a6d3710b7a39fd91fd84b2bf02392820c449
Andrey <suspendwinlogon@gmail.com>
2019-02-02 14:31:11 +0700

    Fixed missed "namespace::" in geometry.cpp

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -5,31 +5,31 @@ MathAn::Vector2() {
   x = 1;
   y = 1;
 }
-Vector2(const double xi, const double yi) {
+MathAn::Vector2(const double xi, const double yi) {
   x = xi;
   y = yi;
 }
-Vector2(const Vector2 i) {
+MathAn::Vector2(const MathAn::Vector2 i) {
   x = i.x;
   y = i.y;
 }
 double get_length() const {
   return hypot(x, y);
 }
-Vector2 get_normalised() const {
+MathAn::Vector2 get_normalised() const {
   double len = get_length();
   x /= len;
   y /= len;
 }
-Vector2 operator+(const Vector2 other) const {
+MathAn::Vector2 operator+(const MathAn::Vector2 other) const {
   return Vector2(x + other.x, y + other.y);
 }
-Vector2 operator-(const Vector2 other) const {
+MathAn::Vector2 operator-(const MathAn::Vector2 other) const {
   return Vector2(x - other.x, y - other.y);
 }
-double operator*(const Vector2 other) const {
+double operator*(const MathAn::Vector2 other) const {
   return x * other.x + y * other.y;
 }
-double get_angle(const Vector2 other) const {
+double get_angle(const MathAn::Vector2 other) const {
   return (*this) * other / get_length();
 }
