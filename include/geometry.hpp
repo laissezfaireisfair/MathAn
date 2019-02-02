99af0f9cf66db52ddcc51a25395258946ea3791e
Andrey <suspendwinlogon@gmail.com>
2019-02-02 16:18:01 +0700

    Added Point2 class declaration to geometry.hpp

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -18,4 +18,19 @@ namespace MathAn {
     double get_x() const;
     double get_y() const;
   };
+
+  class Point2 {
+    double x, y;
+  public:
+    Point2();
+    Point2(double xi, double yi);
+    Point2(const Point2 & other);
+    double get_x() const;
+    double get_y() const;
+    double get_dist(const Point2 other) const;
+    Vector2 get_movement(const Point2 target) const;
+    Point2 operator=(const Point2 other) const;
+    Point2 operator+(const Vector2 movement) const;
+    Point2 operator+=(const Vector2 movement);
+  }
 };
