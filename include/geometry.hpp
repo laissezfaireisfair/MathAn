ff44b3ff2fe338fb7f70707e1bc00ec8c05edd57
Andrey <suspendwinlogon@gmail.com>
2019-02-02 14:07:14 +0700

    Added Vector2 class to geometry.hpp

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -1,1 +1,17 @@
-namespace MathAn {};
+#pragma once
+
+namespace MathAn {
+  class Vector2 {
+    double x, y;
+  public:
+    Vector2();
+    Vector2(const double xi, const double yi);
+    Vector2(const Vector2 i);
+    Vector2 get_normalised() const;
+    double get_length() const;
+    Vector2 operator+(const Vector2 other) const;
+    Vector2 operator-(const Vector2 other) const;
+    Vector2 operator*(const Vector2 other) const;
+    double get_angle(const Vector2 other) const;
+  };
+};
