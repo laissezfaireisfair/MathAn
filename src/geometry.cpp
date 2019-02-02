cb0846fb13d99f656b10a6f043ac52cfee76bcd7
Andrey <suspendwinlogon@gmail.com>
2019-02-02 14:59:56 +0700

    Fixed namespace mistakes in Vector2 realisation in geometry.cpp

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,41 +1,46 @@
 #pragma once
 #include "./../include/geometry.hpp"
 
-MathAn::Vector2() {
+MathAn::Vector2::Vector2() {
   x = 1;
   y = 1;
 }
-MathAn::Vector2(const double xi, const double yi) {
+MathAn::Vector2::Vector2(const double xi, const double yi) {
   x = xi;
   y = yi;
 }
-MathAn::Vector2(const MathAn::Vector2 i) {
+MathAn::Vector2::Vector2(const MathAn::Vector2 i) {
   x = i.x;
   y = i.y;
 }
-double get_length() const {
+double MathAn::Vector2::get_length() const {
   return hypot(x, y);
 }
-MathAn::Vector2 get_normalised() const {
+MathAn::Vector2 MathAn::Vector2::get_normalised() const {
   double len = get_length();
   x /= len;
   y /= len;
 }
-MathAn::Vector2 operator+(const MathAn::Vector2 other) const {
+MathAn::Vector2 MathAn::Vector2::operator=(const MathAn::Vector2 other) const {
+  x = other.x;
+  y = other.y;
+  return other;
+}
+MathAn::Vector2 MathAn::Vector2::operator+(const MathAn::Vector2 other) const {
   return Vector2(x + other.x, y + other.y);
 }
-MathAn::Vector2 operator-(const MathAn::Vector2 other) const {
+MathAn::Vector2 MathAn::Vector2::operator-(const MathAn::Vector2 other) const {
   return Vector2(x - other.x, y - other.y);
 }
-double operator*(const MathAn::Vector2 other) const {
+double MathAn::Vector2::operator*(const MathAn::Vector2 other) const {
   return x * other.x + y * other.y;
 }
-double get_angle(const MathAn::Vector2 other) const {
+double MathAn::Vector2::get_angle(const MathAn::Vector2 other) const {
   return (*this) * other / get_length();
 }
-double get_x() const {
+double MathAn::Vector2::get_x() const {
   return x;
 }
-double get_y() const {
+double MathAn::Vector2::get_y() const {
   return y;
 }
