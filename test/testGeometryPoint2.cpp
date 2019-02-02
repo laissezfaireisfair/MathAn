c70c60e8f2e4a4f171e8a9faa1b7cc9b6337387a
Andrey <suspendwinlogon@gmail.com>
2019-02-02 16:38:55 +0700

    Added Point2 constructors tests to testGeometryPoint2.cpp

--- test/testGeometryPoint2.cpp
+++ test/testGeometryPoint2.cpp
@@ -5,7 +5,9 @@ using namespace std;
 using namespace MathAn;
 
 int main() {
-  Point2 a;
+  Point2 a, b(1, 2), c(a);
   cout << "a.x = " << a.get_x() << "; a.y = " << a.get_y() << ";" << endl;
+  cout << "b.x = " << b.get_x() << "; b.y = " << b.get_y() << ";" << endl;
+  cout << "c.x = " << c.get_x() << "; c.y = " << c.get_y() << ";" << endl;
   return 0;
 }
