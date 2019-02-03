e15ff1a6a83daa64933ace4b69fae3742129667b
Andrey <suspendwinlogon@gmail.com>
2019-02-03 15:28:56 +0700

    Added Line2 class declaration to geometry.hpp

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -49,4 +49,27 @@ namespace MathAn {
     bool operator==(const Point2 other) const;
     bool operator!=(const Point2 other) const;
   };
+
+  class Line2 {
+    double a, b, c;
+  public:
+    Line2();
+    Line2(const double ai, const double bi, const double ci);
+    Line2(const Line2 & other);
+    Line2(const Point2 pnt, const Vector2 vec);
+    Line2(const Point2 pnt, const Line2 prlll);
+    double get_a() const;
+    double get_b() const;
+    double get_c() const;
+    Point2 get_intersection(const Line2 other) const;
+    Vector2 get_single_direction_vector() const;
+    bool is_point_inside(const Point2 pnt) const;
+    bool is_vector_parallel(const Vector2 vec) const;
+    bool is_line_parallel(const Line2 ln) const;
+    bool is_vector_normal(const Vector2 vec) const;
+    bool is_line_normal(const Line2 ln) const;
+    Line2 operator=(const Line2 other);
+    bool operator==(const Line2 other) const;
+    bool operator!=(const Line2 other) const;
+  }
 };
