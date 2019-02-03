378c62ebbeae80c02a2be44ea94bd864ed35ce53
Andrey <suspendwinlogon@gmail.com>
2019-02-03 17:42:15 +0700

    Added Line2 realisation It haven't been tested

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -156,17 +156,49 @@ MathAn::Line2::Line2(const Point2 pnt, const Line2 prlll) {
   b = prlll.b;
   c = (-1.0) * prlll.a * pnt.get_x() - prlll.b * pnt.get_y();
 }
-double MathAn::Line2::get_a() const;
-double MathAn::Line2::get_b() const;
-double MathAn::Line2::get_c() const;
-MathAn::Point2 MathAn::Line2::get_intersection(const Line2 other) const;
-MathAn::Vector2 MathAn::Line2::get_single_direction_vector() const;
-MathAn::Vector2 MathAn::Line2::get_single_normal_vector() const;
-bool MathAn::Line2::is_point_inside(const Point2 pnt) const;
-bool MathAn::Line2::is_vector_parallel(const Vector2 vec) const;
-bool MathAn::Line2::is_line_parallel(const Line2 ln) const;
-bool MathAn::Line2::is_vector_normal(const Vector2 vec) const;
-bool MathAn::Line2::is_line_normal(const Line2 ln) const;
-MathAn::Line2 MathAn::Line2::operator=(const Line2 other);
-bool MathAn::Line2::operator==(const Line2 other) const;
-bool MathAn::Line2::operator!=(const Line2 other) const;
+double MathAn::Line2::get_a() const {
+  return a;
+}
+double MathAn::Line2::get_b() const {
+  return b;
+}
+double MathAn::Line2::get_c() const {
+  return c;
+}
+MathAn::Point2 MathAn::Line2::get_intersection(const Line2 other) const {
+  double y = (a * other.c + other.a * c) / (a * other.b - other.a * b);
+  double x = ((-1.0) * b - c) / a;
+  return Point2(x, y);
+}
+MathAn::Vector2 MathAn::Line2::get_single_direction_vector() const {
+  return Vector2(-b, a).get_normalised();
+}
+MathAn::Vector2 MathAn::Line2::get_single_normal_vector() const {
+  return Vector2(a, b).get_normalised();
+}
+bool MathAn::Line2::is_point_inside(const Point2 pnt) const {
+  return (a * pnt.get_x() + b * pnt.get_y() + c == 0);
+}
+bool MathAn::Line2::is_vector_parallel(const Vector2 vec) const {
+  return (vec * get_single_normal_vector == 0);
+}
+bool MathAn::Line2::is_vector_normal(const Vector2 vec) const {
+  return (vec * get_single_direction_vector == 0);
+}
+bool MathAn::Line2::is_line_parallel(const Line2 ln) const {
+  return (get_single_normal_vector() * ln.get_single_direction_vector() == 0);
+}
+bool MathAn::Line2::is_line_normal(const Line2 ln) const {
+  return (get_single_normal_vector() * ln.get_single_normal_vector() == 0);
+}
+MathAn::Line2 MathAn::Line2::operator=(const Line2 other) {
+  a = other.a;
+  b = other.b;
+  c = other.c;
+}
+bool MathAn::Line2::operator==(const Line2 other) const {
+  return ((a / other.a == b / other.b) && (b / other.b == c / other.c));
+}
+bool MathAn::Line2::operator!=(const Line2 other) const {
+  return !(*this == other);
+}
