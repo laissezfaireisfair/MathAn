f847ba64fcf7ce7254a80503bdc45e9b6305c719
Andrey <suspendwinlogon@gmail.com>
2019-02-03 18:17:49 +0700

    Added some Line2 tests There are constructor, get_abc() and get_intersection() tests

--- test/Geometry/Line/test1.cpp
+++ test/Geometry/Line/test1.cpp
@@ -1,10 +1,30 @@
 #include "./../../../include/geometry.hpp"
 #include "./../../../src/geometry.cpp"
 #include <iostream>
+#include <cstring>
 using namespace std;
 using namespace MathAn;
 
+void print_line(const Line2 & a, const string name) {
+  cout << "Line " << name << ": " << a.get_a() << "*x + ";
+  cout << a.get_b() << "*y + " << a.get_c() << " = 0;" << endl;
+}
+
+void print_pnt(const Point2 & a, const string name) {
+  cout << "Point " << name << " (" << a.get_x() << "; ";
+  cout << a.get_y() << ");" << endl;
+}
+
+void print_vec(const Vector2 & a, const string name) {
+  cout << "Vector " << name << " (" << a.get_x() << "; ";
+  cout << a.get_y() << ");" << endl;
+}
+
 int main() {
-  Line2 a;
+  Line2 a, b(1, 2, -3), c(a);
+  print_line(a, "A");
+  print_line(b, "B");
+  print_line(c, "C");
+  print_pnt(a.get_intersection(b), "(a and b intersection)");
   return 0;
 }
