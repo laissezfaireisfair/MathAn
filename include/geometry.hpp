cabf0c174a24d5b2ad01234dba184e5ffc4f041f
suspendwinlogon <suspendwinlogon@gmail.com>
2019-02-05 16:19:39 +0700

    Add Circle2 class declaration
    The class depends on Point2, Vector2.

--- include/geometry.hpp
+++ include/geometry.hpp
@@ -75,4 +75,29 @@ namespace MathAn {
     bool operator==(const Line2 other) const;
     bool operator!=(const Line2 other) const;
   };
+
+  class Circle2 {
+    Point2 centre;
+    double radius;
+  public:
+    Circle2();
+    Circle2(Point2 cntri, double radi);
+    Circle2(const Circle2 & other);
+    Point2 get_centre() const;
+    double get_radius() const;
+    Circle2 operator=(const Circle2 other);
+    bool operator==(const Circle2 other) const;
+    bool operator!=(const Circle2 other) const;
+    bool operator>(const Circle2 other) const;
+    bool operator<(const Circle2 other) const;
+    bool operator>=(const Circle2 other) const;
+    bool operator<=(const Circle2 other) const;
+    bool is_inside(const Point2 pnt) const;
+    bool is_on_the_border(const Point2 pnt) const;
+    bool is_outside(const Point2 pnt) const;
+    bool is_match(const Circle2 other) const;
+    bool is_cross(const Circle2 other) const;
+    bool is_touch(const Circle2 other) const;
+    Circle2 get_moved(const Vector2 movement) const;
+  };
 };
