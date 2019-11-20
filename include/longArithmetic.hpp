bfeefadc80be9fc7c41b468bcda90647475e2ac1
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-11-20 23:19:55 +0700

    Add long arithmetic header

---
+++ include/longArithmetic.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+namespace MathAn {
+  class LongNum {
+    vector<bool> body;
+    short sign;
+  public:
+    LongNum();
+    LongNum(LongNum const & other);
+    LongNum(int const num);
+    LongNum(std::string const strNum);
+    int get_int(unsigned int const notation = 10) const;
+    std::string get_string(unsigned int const notation = 10) const;
+    short get_sign() const;
+    unsigned int get_int_digit(unsigned int const numDigit,\
+        unsigned int const notation = 10) const;
+    std::string get_string_digit(unsigned int const numDigit,\
+        unsigned int const notation = 10) const;
+    LongNum get_abs() const;
+    LongNum get_negative() const;
+    LongNum operator+(LongNum const other) const;
+    LongNum operator-(LongNum const other) const;
+    LongNum operator*(LongNum const other) const;
+    LongNum operator/(LongNum const other) const;
+    LongNum operator+=(LongNum const other);
+    LongNum operator-=(LongNum const other);
+    LongNum operator*=(LongNum const other);
+    LongNum operator/=(LongNum const other);
+    bool operator==(LongNum const other) const;
+    bool operator!=(LongNum const other) const;
+    bool operator>(LongNum const other) const;
+    bool operator<(LongNum const other) const;
+    bool operator>=(LongNum const other) const;
+    bool operator<=(LongNum const other) const;
+  };
+}
