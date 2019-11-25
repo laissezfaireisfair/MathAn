60f9fb24daf5507ae53e3d6bfa77682af9497435
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-11-25 18:05:45 +0700

    Fix fractions and fraction test

--- test/fractions/fractions_test_1.cpp
+++ test/fractions/fractions_test_1.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-  MathAn::Fraction<int> a(1, 2), b(2, 3);
+  MathAn::Fraction a(1, 2), b(2, 3);
   cout << "a = " << a.get_str() << "; b = " << b.get_str() << ";" << endl;
-  cout << "a + b = " << MathAn::Fraction<int>(a + b).get_str() << ";" << endl;
-  cout << "a - b = " << MathAn::Fraction<int>(a - b).get_str() << ";" << endl;
-  cout << "a * b = " << MathAn::Fraction<int>(a * b).get_str() << ";" << endl;
-  cout << "a / b = " << MathAn::Fraction<int>(a / b).get_str() << ";" << endl;
+  cout << "a + b = " << MathAn::Fraction(a + b).get_str() << ";" << endl;
+  cout << "a - b = " << MathAn::Fraction(a - b).get_str() << ";" << endl;
+  cout << "a * b = " << MathAn::Fraction(a * b).get_str() << ";" << endl;
+  cout << "a / b = " << MathAn::Fraction(a / b).get_str() << ";" << endl;
   cout << "a > b = " << (a > b ? "true" : "false") << ";" << endl;
   cout << "a < b = " << (a < b ? "true" : "false") << ";" << endl;
   cout << "a >= b = " << (a >= b ? "true" : "false") << ";" << endl;
