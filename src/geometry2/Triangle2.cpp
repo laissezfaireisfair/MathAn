c1ea2d523f70d67c1ddcd484a4d9724bc355637a
Andrey Golubev <laissezfaireisfair@protonmail.com>
2019-12-08 16:19:35 +0700

    Move geometry2 realisation to namespace MathAn
    It was done to shorten methods signatures

--- src/geometry2/Triangle2.cpp
+++ src/geometry2/Triangle2.cpp
@@ -1,85 +1,87 @@
 #pragma once
 #include "./../../include/geometry2/Triangle2.hpp"
 
-MathAn::Triangle2::Triangle2() {
-  a = Point2(0, 0);
-  b = Point2(1, 1);
-  c = Point2(2, 0);
-}
+namespace MathAn {
+  Triangle2::Triangle2() {
+    a = Point2(0, 0);
+    b = Point2(1, 1);
+    c = Point2(2, 0);
+  }
 
-MathAn::Triangle2::Triangle2(const Point2 ai, const Point2 bi, const Point2 ci) {
-  a = ai;
-  b = bi;
-  c = ci;
-}
+  Triangle2::Triangle2(const Point2 ai, const Point2 bi, const Point2 ci) {
+    a = ai;
+    b = bi;
+    c = ci;
+  }
 
-MathAn::Triangle2::Triangle2(const Line2 ln1, const Line2 ln2, const Line2 ln3) {
-  a = ln1.get_intersection(ln2);
-  b = ln2.get_intersection(ln3);
-  c = ln3.get_intersection(ln1);
-}
+  Triangle2::Triangle2(const Line2 ln1, const Line2 ln2, const Line2 ln3) {
+    a = ln1.get_intersection(ln2);
+    b = ln2.get_intersection(ln3);
+    c = ln3.get_intersection(ln1);
+  }
 
-MathAn::Triangle2::Triangle2(const Triangle2 & other) {
-  a = other.a;
-  b = other.b;
-  c = other.c;
-}
+  Triangle2::Triangle2(const Triangle2 & other) {
+    a = other.a;
+    b = other.b;
+    c = other.c;
+  }
 
-MathAn::Point2 MathAn::Triangle2::get_a() const {
-  return a;
-}
+  Point2 Triangle2::get_a() const {
+    return a;
+  }
 
-MathAn::Point2 MathAn::Triangle2::get_b() const {
-  return b;
-}
+  Point2 Triangle2::get_b() const {
+    return b;
+  }
 
-MathAn::Point2 MathAn::Triangle2::get_c() const {
-  return c;
-}
+  Point2 Triangle2::get_c() const {
+    return c;
+  }
 
-MathAn::Triangle2 MathAn::Triangle2::operator=(const Triangle2 other) {
-  a = other.a;
-  b = other.b;
-  c = other.c;
-  return other;
-}
+  Triangle2 Triangle2::operator=(const Triangle2 other) {
+    a = other.a;
+    b = other.b;
+    c = other.c;
+    return other;
+  }
 
-bool MathAn::Triangle2::operator==(const Triangle2 other) const{
-  if (a.get_movement(b) != other.a.get_movement(other.b))
-    return false;
-  if (a.get_movement(c) != other.a.get_movement(other.c))
-    return false;
-  return true;
-}
-
-bool MathAn::Triangle2::operator!=(const Triangle2 other) const{
-  return !((*this) == other);
-}
-
-bool MathAn::Triangle2::is_on_the_border(const Point2 pnt) const {
-  if (Line2(a, b).is_point_inside(pnt))
+  bool Triangle2::operator==(const Triangle2 other) const{
+    if (a.get_movement(b) != other.a.get_movement(other.b))
+      return false;
+    if (a.get_movement(c) != other.a.get_movement(other.c))
+      return false;
     return true;
-  if (Line2(b, c).is_point_inside(pnt))
-    return true;
-  if (Line2(c, a).is_point_inside(pnt))
-    return true;
-  return false;
-}
+  }
 
-MathAn::Triangle2 MathAn::Triangle2::get_moved(const Vector2 movement) const {
-  return Triangle2(a + movement, b + movement, c + movement);
-}
+  bool Triangle2::operator!=(const Triangle2 other) const{
+    return !((*this) == other);
+  }
 
-double MathAn::Triangle2::count_square() const {
-  double t1 = a.get_dist(b), t2 = b.get_dist(c), t3 = c.get_dist(a);
-  double p = (t1 + t2 + t3) / 2;
-  return sqrt(p*(p-t1)*(p-t2)*(p-t3));
-}
-
-bool MathAn::Triangle2::is_inside(const Point2 pnt) const {
-  if (is_on_the_border(pnt))
+  bool Triangle2::is_on_the_border(const Point2 pnt) const {
+    if (Line2(a, b).is_point_inside(pnt))
+      return true;
+    if (Line2(b, c).is_point_inside(pnt))
+      return true;
+    if (Line2(c, a).is_point_inside(pnt))
+      return true;
     return false;
-  Triangle2 t1(a, b, pnt), t2(a, c, pnt), t3(b, c, pnt);
-  double s = t1.count_square() + t2.count_square() + t3.count_square();
-  return count_square() == s;
+  }
+
+  Triangle2 Triangle2::get_moved(const Vector2 movement) const {
+    return Triangle2(a + movement, b + movement, c + movement);
+  }
+
+  double Triangle2::count_square() const {
+    double t1 = a.get_dist(b), t2 = b.get_dist(c), t3 = c.get_dist(a);
+    double p = (t1 + t2 + t3) / 2;
+    return sqrt(p*(p-t1)*(p-t2)*(p-t3));
+  }
+
+  bool Triangle2::is_inside(const Point2 pnt) const {
+    if (is_on_the_border(pnt))
+      return false;
+    Triangle2 t1(a, b, pnt), t2(a, c, pnt), t3(b, c, pnt);
+    double s = t1.count_square() + t2.count_square() + t3.count_square();
+    return count_square() == s;
+  }
 }
--- src/geometry2/Vector2.cpp
+++ src/geometry2/Vector2.cpp
@@ -1,112 +1,114 @@
 #pragma once
 #include "./../../include/geometry2/Vector2.hpp"
 
-MathAn::Vector2::Vector2() {
-  x = 1;
-  y = 1;
-}
+namespace MathAn {
+  Vector2::Vector2() {
+    x = 1;
+    y = 1;
+  }
 
-MathAn::Vector2::Vector2(const double xi, const double yi) {
-  x = xi;
-  y = yi;
-}
+  Vector2::Vector2(const double xi, const double yi) {
+    x = xi;
+    y = yi;
+  }
 
-MathAn::Vector2::Vector2(const MathAn::Vector2 & i) {
-  x = i.x;
-  y = i.y;
-}
+  Vector2::Vector2(const Vector2 & i) {
+    x = i.x;
+    y = i.y;
+  }
 
-double MathAn::Vector2::get_length() const {
-  return hypot(x, y);
-}
+  double Vector2::get_length() const {
+    return hypot(x, y);
+  }
 
-MathAn::Vector2 MathAn::Vector2::get_normalised() const {
-  double len = get_length();
-  return Vector2(x/len, y/len);
-}
+  Vector2 Vector2::get_normalised() const {
+    double len = get_length();
+    return Vector2(x/len, y/len);
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator=(const MathAn::Vector2 other){
-  x = other.x;
-  y = other.y;
-  return other;
-}
+  Vector2 Vector2::operator=(const Vector2 other){
+    x = other.x;
+    y = other.y;
+    return other;
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator+(const MathAn::Vector2 other) const {
-  return Vector2(x + other.x, y + other.y);
-}
+  Vector2 Vector2::operator+(const Vector2 other) const {
+    return Vector2(x + other.x, y + other.y);
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator-(const MathAn::Vector2 other) const {
-  return Vector2(x - other.x, y - other.y);
-}
+  Vector2 Vector2::operator-(const Vector2 other) const {
+    return Vector2(x - other.x, y - other.y);
+  }
 
-double MathAn::Vector2::operator*(const MathAn::Vector2 other) const {
-  return x * other.x + y * other.y;
-}
+  double Vector2::operator*(const Vector2 other) const {
+    return x * other.x + y * other.y;
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator+=(const Vector2 other) {
-  x += other.x;
-  y += other.y;
-  return (*this);
-}
+  Vector2 Vector2::operator+=(const Vector2 other) {
+    x += other.x;
+    y += other.y;
+    return (*this);
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator-=(const Vector2 other) {
-  x -= other.x;
-  y -= other.y;
-  return (*this);
-}
+  Vector2 Vector2::operator-=(const Vector2 other) {
+    x -= other.x;
+    y -= other.y;
+    return (*this);
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator*=(const double num) {
-  x *= num;
-  y *= num;
-  return (*this);
-}
+  Vector2 Vector2::operator*=(const double num) {
+    x *= num;
+    y *= num;
+    return (*this);
+  }
 
-MathAn::Vector2 MathAn::Vector2::operator/=(const double num) {
-  x /= num;
-  y /= num;
-  return (*this);
-}
+  Vector2 Vector2::operator/=(const double num) {
+    x /= num;
+    y /= num;
+    return (*this);
+  }
 
-double MathAn::Vector2::get_angle(const MathAn::Vector2 other) const {
-  return acos((*this) * other / get_length() / other.get_length());
-}
+  double Vector2::get_angle(const Vector2 other) const {
+    return acos((*this) * other / get_length() / other.get_length());
+  }
 
-double MathAn::Vector2::get_x() const {
-  return x;
-}
+  double Vector2::get_x() const {
+    return x;
+  }
 
-double MathAn::Vector2::get_y() const {
-  return y;
-}
+  double Vector2::get_y() const {
+    return y;
+  }
 
-bool MathAn::Vector2::operator==(const Vector2 other) const {
-  return (x == other.x) && (y == other.y);
-}
+  bool Vector2::operator==(const Vector2 other) const {
+    return (x == other.x) && (y == other.y);
+  }
 
-bool MathAn::Vector2::operator!=(const Vector2 other) const {
-  return (x != other.x) || (y != other.y);
-}
+  bool Vector2::operator!=(const Vector2 other) const {
+    return (x != other.x) || (y != other.y);
+  }
 
-MathAn::Vector2 MathAn::operator*(const Vector2 vec, const double num) {
-  return Vector2(vec.x * num, vec.y * num);
-}
+  Vector2 operator*(const Vector2 vec, const double num) {
+    return Vector2(vec.x * num, vec.y * num);
+  }
 
-MathAn::Vector2 MathAn::operator*(const double num, const Vector2 vec) {
-  return Vector2(vec.x * num, vec.y * num);
-}
+  Vector2 operator*(const double num, const Vector2 vec) {
+    return Vector2(vec.x * num, vec.y * num);
+  }
 
-MathAn::Vector2 MathAn::operator/(const Vector2 vec, const double num) {
-  return Vector2(vec.x / num, vec.y / num);
-}
+  Vector2 operator/(const Vector2 vec, const double num) {
+    return Vector2(vec.x / num, vec.y / num);
+  }
 
-MathAn::Vector2 MathAn::operator/(const double num, const Vector2 vec) {
-  return Vector2(vec.x / num, vec.y / num);
-}
+  Vector2 operator/(const double num, const Vector2 vec) {
+    return Vector2(vec.x / num, vec.y / num);
+  }
 
-bool MathAn::Vector2::is_vector_collinear(const Vector2 other) const {
-  return ((*this) * Vector2((-other.y), other.x) == 0);
-}
+  bool Vector2::is_vector_collinear(const Vector2 other) const {
+    return ((*this) * Vector2((-other.y), other.x) == 0);
+  }
 
-bool MathAn::Vector2::is_vector_normal(const Vector2 other) const {
-  return ((*this) * other == 0);
+  bool Vector2::is_vector_normal(const Vector2 other) const {
+    return ((*this) * other == 0);
+  }
 }
